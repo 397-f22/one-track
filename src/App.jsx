5cd67ad47a43d26d349ae536417bbc87320b5548
kabaniak <kabaniak@gmail.com>
2022-11-01 18:07:07 -0500

    firebase stuff + card update

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,28 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { useState } from "react";
-import PackageCardList from "./components/PackageCardList";
-import AddPackage from "./components/AddPackage";
-import Header from "./components/Header";
-import AddPackageForm from "./components/addPackageForm";
-import Search from "./components/Search";
 
-const initData = {
-	"1Z12345E0205271688": {
-		"carrier": "UPS",
-		"package_name": "shoes",
-		"estimated_delivery": "2021-09-20T14:03:00",
-		"last_checkpoint_location": "Skokie, IL",
-		"response": "In Transit"
-	},
-	"1Z12345E6605272234": {
-		"carrier": "UPS",
-		"package_name": "candy",
-		"estimated_delivery": "2021-09-20T14:03:00",
-		"last_checkpoint_location": "Chicago, IL",
-		"response": "In Transit"
-	},
-	"1Z12345E0305271640": {
-		"carrier": "Amazon",
-		"package_name": "white shirt",
-		"estimated_delivery": "2021-09-20T14:03:00",
-		"last_checkpoint_location": "Skokie, IL",
-		"response": "Delivered"
-	}
-}
+import { useDbData } from "./utilities/firesbase";
+import Main from "./components/Main";
 
+// temporary and should be changed when we add more users
+const uid = "12345678";
 
 const App = () => {
-  // const [data, error] = useDbData("/");
-  // if (error) return <h1>Error loading data: {error.toString()}</h1>;
-  // if (data === undefined) return <h1>Loading data...</h1>;
-  // if (!data) return <h1>No data found</h1>;
-  	const [data, setData] = useState(initData);
-	const [packages, setPackages] = useState(initData);
-	const [open, setOpen] = useState(false);
+	const [userData, error] = useDbData(`/users/${uid}`);
+	const [imageData, imageError] = useDbData(`/images`);
 
-	const openModal = () => setOpen(true);
-	const closeModal = () => setOpen(false);
+	if (error) return <h1>Error loading data: {error.toString()}</h1>;
+	if (userData === undefined) return <h1>Loading data...</h1>;
+	if (!userData) return <h1>No data found</h1>;
 
-  return (
-		<div className="container">
-			<Header />
-			<Search data={data} setPackages={setPackages}/>
-			<PackageCardList packages={packages} />
+	
+	if (imageError) return <h1>Error loading data: {imageError.toString()}</h1>;
+	if (imageData === undefined) return <h1>Loading data...</h1>;
+	if (!imageData) return <h1>No data found</h1>;
 
-			<button type="button" className="btn btn-outline-danger" onClick={openModal}>
-				Add Package
-			</button>
-			<AddPackage open={open} close={closeModal}>
-				<AddPackageForm setData={setData} data={data} setPackages={setPackages}/>
-			</AddPackage>
-		</div>
+	return (
+		<Main userData={userData} imageData={imageData}></Main>
 	);
 };
 
